31a60acfa159ac41fe45ada841f0385d197c490c
ABHISHEK VERMA <96565154+abhishekverma276@users.noreply.github.com>
2025-02-15 12:49:01 +0530

    Update checkwallet.js

--- checkwallet.js
+++ checkwallet.js
@@ -1,7 +1,7 @@
 const { ethers } = require("hardhat");
 
 async function main() {
-  const privateKey = "0x065f0eb0a4b2408bbeac1c66173dac7f92973d3b7508d28f273be32041367a23"; // Replace with your private key
+  const privateKey = "key"; // Replace with your private key
   const wallet = new ethers.Wallet(privateKey);
   console.log("Wallet address:", wallet.address);
 }
